36b3e6cdd7d6466c16f5108b4751aed501c540fe
6MIL9 <emilkliavlin2@gmail.com>
2020-09-12 18:49:22 +0500

    add react

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,38 +1,89 @@
 const { Router } = require('express');
-const {check, validationResult} = require('express-validator')
+const jwt = require('jsonwebtoken')
+const { check, validationResult } = require('express-validator')
 const bcrypt = require('bcrypt');
+const config = require('../config/default.json')
 const User = require('../models/User')
 const router = Router();
 
 // /api/auth/register
-router.post('/register', async (req, res) => {
-    try {
-        const { email, password } = req.body
+router.post('/register',
+    [
+        check('email', 'Incorrect email').isEmail(),
+        check('password', 'Incorrect password, min symbols - 6')
+            .isLength({ min: 6 })
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req)
 
-        const candidate = await User.findOne({ email })
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'Incorrect data'
+                })
+            }
 
-        if (candidate) {
-            return res.status(400).json({ message: 'User already exists' })
+            const { email, password } = req.body
+
+            const candidate = await User.findOne({ email })
+
+            if (candidate) {
+                return res.status(400).json({ message: 'User already exists' })
+            }
+
+            const hashedPassword = await bcrypt.hash(password, 12)
+            const user = new User({ email, password: hashedPassword })
+
+            await user.save()
+
+            res.status(201).json({ message: 'User registered' })
+        } catch (e) {
+            res.status(500).json({ message: 'something wrong :(' })
         }
-
-        const hashedPassword = await bcrypt.hash(password, 12)
-        const user = new User({ email, password: hashedPassword })
-
-        await user.save()
-
-        res.status(201).json({ message: 'User registered' })
-    } catch (e) {
-        res.status(500).json({ message: 'something wrong :(' })
-    }
-})
+    })
 
 // /api/auth/login
-router.post('/login', async (req, res) => {
-    try {
+router.post('/login',
+    [
+        check('email', 'Incorrect email').normalizeEmail().isEmail(),
+        check('password', 'Incorrect password, min symbols - 6').exists()
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req)
 
-    } catch (e) {
-        res.status(500).json({ message: 'something wrong :(' })
-    }
-})
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'Login failed'
+                })
+            }
+
+            const { email, password } = req.body
+
+            const user = await User.findOne({ email })
+
+            if (!user) {
+                return res.status(400).json({ message: 'User not found' })
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password)
+
+            if (!isMatch) {
+                return res.status(400).json({ message: 'Password incorrect' })
+            }
+
+            const token = jwt.sign(
+                { userId: user.id },
+                config.get('jwtSecret'),
+                { expiresIn: '1h' }
+            )
+
+            res.json({ token, userId })
+        } catch (e) {
+            res.status(500).json({ message: 'something wrong :(' })
+        }
+    })
 
 module.exports = router;
