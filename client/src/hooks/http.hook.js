86bdeaaf0888cb18440b2a8fd86596cd000145a6
6MIL9 <emilkliavlin2@gmail.com>
2020-09-13 21:56:10 +0500

    add routes

---
+++ client/src/hooks/http.hook.js
@@ -0,0 +1,12 @@
+import { useState, useCallback } from 'react'
+
+export const useHttp = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const request = useCallback(async (url, method = "GET", body = null, headers = {}) => {
+
+    })
+
+    return { loading, request, error }
+}
--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AuthPage = () => {
+
+    const [form, setForm] = useState({
+        email: '', password: ''
+    })
+
+    const changeHandler = (event) => {
+        setForm({ ...form, [event.target.name]: event.target.value })
+    }
+
     return (
-        <div>
-            <h1>AuthPage</h1>
+        <div className="row">
+            <div className="col s6 offset-s3">
+                <h1>Сократи ссылку</h1>
+                <div className="card blue darken-5">
+                    <div className="card-content white-text">
+                        <span className="card-title">Авторизация</span>
+                        <div>
+                            <div className="input-field">
+                                <input placeholder="Введите email" id="email" type="text" name="email" className="yellow-input" onChange={changeHandler} />
+                                <label htmlFor="email">Email</label>
+                            </div>
+                            <div className="input-field">
+                                <input placeholder="Введите пароль" id="password" type="password" name="password" className="yellow-input" onChange={changeHandler} />
+                                <label htmlFor="password">Password</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-action">
+                        <button className="btn green" style={{ marginRight: "1rem" }}>Войти</button>
+                        <button className="btn yellow black-text">Регистрация</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
